def1fd934ab331877f1c622900c5b24b3293127b
Eniola3321 <ekanadeadeniyiabdulhafis@gmail.com>
2026-08-22 14:14:51 +0100

    feat: all issues has been completed

--- src/earnings/anomaly-detection.queue.ts
+++
@@ -1,6 +0,0 @@
-export const ANOMALY_DETECTION_QUEUE = 'anomaly-detection';
-
-/**
- * Analytics/anomaly detection is a background task and can be queued lower.
- */
-export const ANOMALY_DETECTION_QUEUE_PRIORITY = 20;
