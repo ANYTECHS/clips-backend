5d4ea98c0fe0f7270b3f6dbd57390aeeb35d3554
OBAZE SAMUEL OSHIOKE <samuel.m2301052@st.futminna.edu.ng>
2026-08-25 09:56:38 +0100

    refactor: import EarningsModule in PayoutsModule

--- src/payouts/payouts.module.ts
+++ src/payouts/payouts.module.ts
@@ -19,6 +19,7 @@ import { StellarConfirmationProcessor } from './stellar-confirmation.processor';
 import { STELLAR_CONFIRMATION_QUEUE } from './stellar-confirmation.queue';
 import { PayoutApprovalService } from './payout-approval.service';
 import { ConfigService } from '../config/config.service';
+import { EarningsModule } from '../earnings/earnings.module';
 
 @Module({
   imports: [
@@ -27,6 +28,7 @@ import { ConfigService } from '../config/config.service';
     AuthModule,
     EncryptionModule,
     MetricsModule,
+    EarningsModule,
     BullModule.registerQueue({
       name: PAYOUT_RETRY_QUEUE,
       defaultJobOptions: { priority: PAYOUT_RETRY_QUEUE_PRIORITY },
